7229951d666ac27c398aa805da0cb79312752e2c
nvlanh1704 <nvietlanh@gmail.com>
2026-09-26 05:54:27 +0700

    Buoi03 Bai02 tinh dien tich

---
+++ NguyenVietLanh-25810026-Buoi03-Functions/Bai02_TinhDienTich.kt
@@ -0,0 +1,11 @@
+//Nguyen Viet Lanh 25810026
+
+fun tinhDienTich(chieuDai: Double, chieuRong: Double): Double {
+    return chieuDai * chieuRong
+}
+
+fun main() {
+    println("Dien tich 1: ${tinhDienTich(5.0, 3.0)}")
+    println("Dien tich 2: ${tinhDienTich(8.0, 4.0)}")
+}
+
