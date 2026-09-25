75d5236c7da41ca27ce7adb86fb0dd74a1e52bc8
nvlanh1704 <nvietlanh@gmail.com>
2026-09-26 05:54:39 +0700

    Buoi03 Bai03 ghi nhat ky

---
+++ NguyenVietLanh-25810026-Buoi03-Functions/Bai03_GhiNhatKy.kt
@@ -0,0 +1,14 @@
+//Nguyen Viet Lanh 25810026
+
+fun ghiNhatKy1(hanhDong: String): Unit {
+    println("Nhat ky: $hanhDong")
+}
+
+fun ghiNhatKy2(hanhDong: String) {
+    println("Nhat ky: $hanhDong")
+}
+
+fun main() {
+    ghiNhatKy1("Dang nhap")
+    ghiNhatKy2("Dang xuat")
+}
